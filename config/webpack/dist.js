b79f54f369053fc80f9949d68d13e9cad2054329
Ryan Burnette <ryan.burnette@gmail.com>
2018-03-22 01:12:36 -0400

    webpack config files

--- config/webpack/dist.js
+++ config/webpack/dist.js
@@ -1,10 +1,10 @@
-/* jshint node: true, asi: true, esversion: 6 */
 const path = require('path')
+const Uglify = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: './lib/faa-endorsements.js',
+  entry: path.resolve(__dirname,'../../src/faa-endorsements.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname,'../../dist'),
     filename: 'faa-endorsements.js',
     library: 'FAAEndorsements',
     libraryExport: 'default',
@@ -14,11 +14,14 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader'
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new Uglify()
+  ]
 }
