764b96dc89339621748fed72c184fdc9674af18e
Michael McCracken <michael.mccracken172@gmail.com>
2018-03-22 16:23:19 -0400

    fixed-tests

--- src/faa-endorsements.js
+++ src/faa-endorsements.js
@@ -1,5 +1,8 @@
+import bar from '../templates/a-3-pre-solo-knowledge.md'
+
 export class FAAEndorsements {
-  constructor() {
-    this.foo = "foo"
+  constructor () {
+    this.foo = 'foo'
+    this.bar = bar
   }
 }
