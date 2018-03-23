31097f1a6341e905e6d752998853da01eefb3a25
Ryan Burnette <ryan.burnette@gmail.com>
2018-03-23 13:26:54 -0400

    fix dist build issue

--- config/webpack/resolve.js
+++ config/webpack/resolve.js
@@ -2,7 +2,8 @@ const path = require('path')
 
 const resolve = {
   alias: {
-    Templates: path.resolve(__dirname, '../../src/templates/')
+    Templates: path.resolve(__dirname,'../../src/templates/'),
+    handlebars: path.resolve(__dirname,'../../node_modules/handlebars/dist/handlebars.js')
   }
 }
 
