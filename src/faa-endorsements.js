402c7cead9a23eeb4dd87ff1a652ee1e399798de
Ryan Burnette <ryan.burnette@gmail.com>
2018-03-24 01:31:37 -0400

    more progress

--- src/faa-endorsements.js
+++ src/faa-endorsements.js
@@ -2,32 +2,42 @@ import filter from 'lodash/filter'
 import merge from 'lodash/merge'
 import each from 'lodash/each'
 import Handlebars from 'handlebars'
+import arrayToObjectTemplate from 'array-to-object-template'
 
 import {Templates,Endorsements} from 'Templates'
 
 export class FAAEndorsements {
   constructor (options={}) {
+    this.options = options
+    setDefaults(this)
     this._endorsements = []
-    this.endorsements = []
+    this.endorsementTemplates = []
     this.locals = {}
 
-    if ( typeof options.missing !== 'undefined' ) {
-      this.missing = options.missing
-    }
-    else {
-      this.missing = '<span class="missing"></span>'
-    }
-
-    if ( options.endorsements ) {
+    if ( Array.isArray(options.endorsements) )  {
       each(options.endorsements,title => addEndorsement(title))
     }
   }
 
   addEndorsement(title) {
-    let template = getTemplate(title)
+    let template = FAAEndorsements.getTemplate(title)
     if ( template ) {
-
+      this.endorsementTemplates.push(template)
+      this.endorsements.push(template.attributes.title)
+      setLocals(this,template)
     }
+    else {
+      throw new Error(`template ${title} not be found`)
+    }
+  }
+
+  set endorsements(es) {
+    this._endorsements = es
+  }
+
+  get endorsements() {
+    console.log('foo')
+    return this._endorsements
   }
 
   static get Templates() {
@@ -45,5 +55,35 @@ export class FAAEndorsements {
 
   static get Endorsements() {
     return Endorsements;
+  }
+}
+
+function setLocals(_this,template) {
+  _this.locals = merge(arrayToObjectTemplate(template.attributes.locals),_this.locals)
+  if ( _this.options.missing ) {
+    applyMissingToLocals(_this)
+  }
+}
+
+function _applyMissingToLocals(_this,obj) {
+  let newObj = {}
+  Object.keys(obj).forEach(k => {
+    if ( !obj[k] ) {
+      newObj[k] = _this.options.missing
+    }
+    else {
+      newObj[k] = _applyMissingToLocals(_this,obj[k])
+    }
+  })
+  return newObj
+}
+
+function applyMissingToLocals(_this) {
+  _this.locals = _applyMissingToLocals(_this,_this.locals)
+}
+
+function setDefaults(_this) {
+  if ( !_this.options.missing ) {
+    _this.options.missing = null
   }
 }
--- tests/faa-endorsements.js
+++ tests/faa-endorsements.js
@@ -31,4 +31,28 @@ describe('FAAEndorsements()',() => {
       expect(FAAEndorsements.getTemplate()).to.be.an('undefined')
     })
   })
+
+  context('.addEndorsement()',() => {
+    it('adds an endorsement to the instance',() => {
+      let f = new FAAEndorsements()
+      f.addEndorsement(FAAEndorsements.Endorsements[0])
+      expect(f.endorsements.length).to.be.gt(0)
+    })
+
+    it('updates the locals',() => {
+      let f = new FAAEndorsements()
+      f.addEndorsement(FAAEndorsements.Endorsements[0])
+      expect(f.locals).to.be.an('object')
+      expect(f.locals).to.include.keys(['date','student','instructor','aircraft'])
+      expect(Object.keys(f.locals).length).to.be.gt(0)
+    })
+  })
+
+  context('custom missing', () => {
+    it('sets a custom missing string',() => {
+      let f = new FAAEndorsements({ missing: 'foo' })
+      f.addEndorsement(FAAEndorsements.Endorsements[0])
+      expect(f.locals.date).to.eq('foo')
+    })
+  })
 })
